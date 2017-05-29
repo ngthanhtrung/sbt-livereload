6470bbeebdf6237d51110e676ef48e4c3c14f89b
Trung Nguyen <i@ngthanhtrung.com>
2017-05-29 16:05:18 +0700

    Start/stop manually

--- src/main/scala/sbtlivereload/LiveReloadPlugin.scala
+++ src/main/scala/sbtlivereload/LiveReloadPlugin.scala
@@ -6,9 +6,13 @@ import sbt._
 object LiveReloadPlugin extends AutoPlugin { self =>
 
   object autoImport {
-    lazy val liveReloadEnabled = settingKey[Boolean]("Live-reload enabled.")
     lazy val liveReloadServerHost = settingKey[String]("Live-reload server host.")
     lazy val liveReloadServerPort = settingKey[Int]("Live-reload server port.")
+
+    lazy val startLiveReloadAtLoad = settingKey[Boolean]("Start Live-reload at load.")
+    lazy val startLiveReload = taskKey[Unit]("Start live-reload.")
+    lazy val stopLiveReload = taskKey[Unit]("Stop live-reload.")
+
     lazy val reloadStylesheets = taskKey[Unit]("Live-reload stylesheets.")
     lazy val reloadPage = taskKey[Unit]("Live-reload page.")
   }
@@ -17,37 +21,50 @@ object LiveReloadPlugin extends AutoPlugin { self =>
 
   private var server: Option[LiveReloadServer] = None
 
+  private def start(host: String, port: Int): Unit = {
+    if (server.isEmpty) {
+      self.synchronized {
+        if (server.isEmpty) {
+          server = Some(
+            new LiveReloadServer(host, port)
+          )
+        }
+      }
+    }
+  }
+
+  private def stop(): Unit = {
+    if (server.isDefined) {
+      self.synchronized {
+        if (server.isDefined) {
+          server.foreach(_.kill())
+          server = None
+        }
+      }
+    }
+  }
+
   override lazy val projectSettings: Seq[Def.Setting[_]] = Seq(
-    liveReloadEnabled := true,
     liveReloadServerHost := "localhost",
     liveReloadServerPort := 27492,
+
+    startLiveReloadAtLoad := false,
+    startLiveReload := start(liveReloadServerHost.value, liveReloadServerPort.value),
+    stopLiveReload := stop(),
+
     reloadStylesheets := server.foreach(_.notify(ReloadStylesheets)),
     reloadPage := server.foreach(_.notify(ReloadPage)),
 
     (onLoad in Global) := (onLoad in Global).value.compose { state =>
-      if (liveReloadEnabled.value && server.isEmpty) {
-        self.synchronized {
-          if (server.isEmpty) {
-            server = Some {
-              new LiveReloadServer(liveReloadServerHost.value, liveReloadServerPort.value)
-            }
-          }
-        }
+      if (startLiveReloadAtLoad.value) {
+        start(liveReloadServerHost.value, liveReloadServerPort.value)
       }
 
       state
     },
 
     (onUnload in Global) := (onUnload in Global).value.compose { state =>
-      if (liveReloadEnabled.value && server.isDefined) {
-        self.synchronized {
-          if (server.isDefined) {
-            server.foreach(_.kill())
-            server = None
-          }
-        }
-      }
-
+      stop()
       state
     }
   )
